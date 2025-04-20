5b99870cd1aecd024f004eb9d72d1b93940e6dac
Mauryan Kansara <mauryankansaramk@gmail.com>
2025-04-20 21:04:00 +0530

    tried fixing cors error 2

--- client/src/game/backend.ts
+++ client/src/game/backend.ts
@@ -1,3 +1,1 @@
-export const BACKEND_URL = import.meta.env.DEV
-    ? "ws://localhost:2567"
-    : import.meta.env.VITE_BACKEND_URL;
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
--- server/src/index.ts
+++ server/src/index.ts
@@ -36,7 +36,7 @@ const server = http.createServer(app);
 server.on("request", (req, res) => {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin || "")) {
-        res.setHeader("Access-Control-Allow-Origin", origin!);
+        res.setHeader("Access-Control-Allow-Origin", "*");
         res.setHeader("Access-Control-Allow-Credentials", "true");
         res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
         res.setHeader("Access-Control-Allow-Headers", "Content-Type");
